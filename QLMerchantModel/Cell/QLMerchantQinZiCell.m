e4afbad33a00e24709b8f6f0dba3a180e4f0e17d
计恩良 <454309007@qq.com>
2019-01-26 00:17:47 +0800

    1
    1

--- QLMerchantModel/Cell/QLMerchantQinZiCell.m
+++ QLMerchantModel/Cell/QLMerchantQinZiCell.m
@@ -8,6 +8,7 @@
 #import "QLMerchantQinZiCell.h"
 #import "WTBaseCore.h"
 #import "QLBusiness.h"
+#import "UIImageView+WebImage.h"
 
 @implementation QLMerchantQinZiItem
 - (id)init{
@@ -55,8 +56,24 @@
         offsetX = 12+(162*xxx)+(12*xxx);
         
         UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMakes(offsetX, offsetY, 162, 160)];
-        btn.backgroundColor = [UIColor redColor];
         [self.contentView addSubview:btn];
+        
+        UIImageView *imageV = [[UIImageView alloc] initWithFrame:CGRectMakes(0, 0, 162, 100)];
+        [imageV setWebImageWithUrl:@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1547976453468&di=0730fb64aadc80b0f2490a430f51aebb&imgtype=0&src=http%3A%2F%2Fimg5.duitang.com%2Fuploads%2Fitem%2F201105%2F31%2F20110531094303_d5JZB.jpg" placeHolder:nil];
+        [btn addSubview:imageV];
+        
+        UILabel *tagLab = [[UILabel alloc] init];
+        tagLab.font = WTFontSys(10*QL_MULPITLE);
+        tagLab.textColor = QL_TagTextColor_Green;
+        tagLab.backgroundColor = QL_TagColor_Green;
+        tagLab.text = @"西餐";
+        tagLab.textAlignment = NSTextAlignmentCenter;
+        [btn addSubview:tagLab];
+        [tagLab sizeToFit];
+        tagLab.left = 20*QL_MULPITLE;
+        tagLab.width = tagLab.width+(10*QL_MULPITLE);
+        tagLab.top = 106*QL_MULPITLE;
+        tagLab.height = 14*QL_MULPITLE;
     }
 }
 
