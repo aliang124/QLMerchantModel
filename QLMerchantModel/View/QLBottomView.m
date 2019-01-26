ee547e245d652eded6a728e202400039874a268b
计恩良 <454309007@qq.com>
2019-01-26 16:29:47 +0800

    1
    1

--- QLMerchantModel/View/QLBottomView.m
+++ QLMerchantModel/View/QLBottomView.m
@@ -14,24 +14,39 @@
     self = [super initWithFrame:frame];
     if (self) {
         float width = (WTScreenWidth-8-8-8)/2;
-        
-        WTCustomButton *leftBtn = [[WTCustomButton alloc] initWithFrame:CGRectMake(8, 8, width, 38)];
-        [self setRounct:leftBtn titleText:@"我要评价"];
+
+        UIButton *leftBtn = [[UIButton alloc] initWithFrame:CGRectMake(8, 8, width, 38)];
+        [self setRounct:leftBtn titleText:@"我要评价" imgIcon:@"QLBusiness/woyaoPingJia"];
         [self addSubview:leftBtn];
         
-        WTCustomButton *rightBtn = [[WTCustomButton alloc] initWithFrame:CGRectMake(leftBtn.right+8, 8, width, 38)];
-        [self setRounct:rightBtn titleText:@"商家主页"];
+        UIButton *rightBtn = [[WTCustomButton alloc] initWithFrame:CGRectMake(leftBtn.right+8, 8, width, 38)];
+        [self setRounct:rightBtn titleText:@"商家主页" imgIcon:@"QLBusiness/seeZhuYe"];
         [self addSubview:rightBtn];
     }
     return self;
 }
 
-- (void)setRounct:(WTCustomButton *)btn titleText:(NSString *)titleText {
+- (void)setRounct:(UIButton *)btn titleText:(NSString *)titleText imgIcon:(NSString *)imgText {
     btn.layer.cornerRadius = 4;
     btn.layer.borderColor = QL_Border_LineColor.CGColor;
     btn.layer.borderWidth = 0.5;
     [btn setBackgroundImage:[WTUtil createImageFromColor:[UIColor whiteColor]] forState:UIControlStateNormal];
     [btn setBackgroundImage:[WTUtil createImageFromColor:WTColorHex(0xFAFAF7)] forState:UIControlStateHighlighted];
     
+    UIImageView *iconImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 9, 20, 20)];
+    [iconImage setImage:[UIImage imageNamed:imgText]];
+    [btn addSubview:iconImage];
+    
+    UILabel *titleLab = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 200, 20)];
+    titleLab.font = WTFontSys(14);
+    titleLab.text = titleText;
+    titleLab.textColor = QL_UserName_TitleColor_Black;
+    [btn addSubview:titleLab];
+    [titleLab sizeToFit];
+    
+    float offset = (btn.width - (20+7+titleLab.width))/2;
+    iconImage.left = offset;
+    titleLab.left = iconImage.right+7;
+    titleLab.top = (btn.height-titleLab.height)/2;
 }
 @end
