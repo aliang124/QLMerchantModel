ddcf5af5859a7193e90641971d303c2638959c54
计恩良 <454309007@qq.com>
2019-01-28 10:29:45 +0800

    1
    1

--- QLMerchantModel/Target_QLMerchantModel.h
+++ QLMerchantModel/Target_QLMerchantModel.h
@@ -9,6 +9,12 @@
 #import <Foundation/Foundation.h>
 
 @interface Target_QLMerchantModel : NSObject
+//跳转到商家列表
 - (UIViewController *)Action_merchantListVC:(NSDictionary*)param;
+//跳转商家详情
 - (UIViewController *)Action_merchantDetailVC:(NSDictionary*)param;
+//跳转到个人主页
+- (UIViewController *)Action_zhuYeVC:(NSDictionary*)param;
+//跳转到评价详情
+- (UIViewController *)Action_pingJiaDetailVC:(NSDictionary*)param;
 @end
